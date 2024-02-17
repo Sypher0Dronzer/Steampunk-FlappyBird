57b2603bf77b622ee92502d63cfed79a5c5d34ba
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2024-02-17 23:48:18 +0530

    screenresize player resize

--- assests/scripts/main.js
+++ assests/scripts/main.js
@@ -6,6 +6,8 @@ class Game{
         this.ctx=context
         this.width=this.canvas.width
         this.height= this.canvas.height
+        this.baseHeight=720
+        this.ratio=this.height/this.baseHeight
         this.player=new Player(this)
 
         this.resize(window.innerWidth,window.innerHeight)
@@ -23,6 +25,9 @@ class Game{
 
         this.width=this.canvas.width
         this.height=this.canvas.height
+        this.ratio=this.height/this.baseHeight
+        this.player.resize()
+
     }
     render(){
     this.player.update()
--- assests/scripts/player.js
+++ assests/scripts/player.js
@@ -3,13 +3,21 @@ class Player{
         this.game=game
         this.x=20
         this.y=50
-        this.height=100
-        this.width=100
+        this.spriteWidth=200;
+        this.spriteHeight=200
+        this.height
+        this.width
+        this.resize()
     }
     draw(){
         this.game.ctx.fillRect(this.x,this.y,this.width,this.height)
     }
     update(){
-        this.x++
+        // this.x++
+    }
+    resize(){
+        //resizing based on height of the canvas
+        this.width=this.spriteWidth * this.game.ratio;
+        this.height=this.spriteHeight * this.game.ratio
     }
 }
