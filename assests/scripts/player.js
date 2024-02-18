d41fde08e3738b79e735b4d7ac7bf7e626c608e4
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2024-02-18 10:51:18 +0530

    mouse controls

--- assests/scripts/player.js
+++ assests/scripts/player.js
@@ -1,35 +1,44 @@
-class Player{
-    constructor(game){
-        this.game=game
-        this.x=20
-        this.y
-        this.spriteWidth=200;
-        this.spriteHeight=200
-        this.height
-        this.width
-        this.speedY
-        
+class Player {
+  constructor(game) {
+    this.game = game;
+    this.x = 20;
+    this.y;
+    this.spriteWidth = 200;
+    this.spriteHeight = 200;
+    this.height;
+    this.width;
+    this.speedY;
+    this.flapSpeed;
+  }
+  draw() {
+    this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  update() {
+    this.y += this.speedY;
+    if (!this.isTouchingBottom()) {
+      this.speedY += this.game.gravity;
     }
-    draw(){
-        this.game.ctx.fillRect(this.x,this.y,this.width,this.height)
+    if (this.isTouchingBottom()) {
+      this.y = this.game.height - this.height;
     }
-    update(){
-        this.y+=this.speedY
-        if(!this.isTouchingBottom()){
-            this.speedY+=this.game.gravity
-        }
-        if(this.isTouchingBottom()){
-this.y=this.game.height-this.height
-        }
+  }
+  resize() {
+    //resizing based on height of the canvas
+    this.width = this.spriteWidth * this.game.ratio;
+    this.height = this.spriteHeight * this.game.ratio;
+    this.y = this.game.height * 0.5 - this.height * 0.5;
+    this.speedY = -5 * this.game.ratio;
+    this.flapSpeed=5*this.game.ratio
+  }
+  isTouchingTop() {
+    return this.y < 0;
+  }
+  isTouchingBottom() {
+    return this.y > this.game.height - this.height;
+  }
+  flap() {
+    if (!this.isTouchingTop()) {
+      this.speedY = -this.flapSpeed;
     }
-    resize(){
-        //resizing based on height of the canvas
-        this.width=this.spriteWidth * this.game.ratio;
-        this.height=this.spriteHeight * this.game.ratio;
-        this.y=this.game.height *0.5 - this.height *0.5
-        this.speedY= -5* this.game.ratio
-    }
-    isTouchingBottom(){
-        return this.y > this.game.height - this.height
-    }
+  }
 }
