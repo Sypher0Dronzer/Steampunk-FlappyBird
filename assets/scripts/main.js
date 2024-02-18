c9b4b7f0335ea4e872a368546ab4760051694e5a
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2024-02-18 23:52:58 +0530

    b4 design

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,9 +14,20 @@ class Game {
     this.numberOfObstacles = 10;
     this.gravity;
     this.speed;
+    this.minSpeed
+    this.maxSpeed
     this.score;
     this.gameOver;
-    this.timer
+    this.timer;
+    this.message1;
+    this.message2;
+    this.barSize
+    //this is used for optimisation since refresh rates are different for different screens and devices
+    this.eventTimer=0;
+    this.eventUpdate=false
+    this.eventInterval=150
+    this.touchStartX;
+    this.swipeDistance=50
 
     this.resize(window.innerWidth, window.innerHeight);
 
@@ -33,25 +44,46 @@ class Game {
       if (e.key == "Enter" || e.key == " ") {
         this.player.flap();
       }
+      if(e.key=="Control"|| e.key=="c" || e.key=="C")     {
+        this.player.startCharge()
+      } 
     });
+    window.addEventListener("keyup", (e) => {
+      console.log(e.key);
+    
+    if(e.key=="Control"|| e.key=="c" || e.key=="C")     {
+      this.player.stopCharge()
+    } 
+  });
     //touch events
     this.canvas.addEventListener("touchstart", (e) => {
       this.player.flap();
+      this.touchStartX=e.changedTouches[0].pageX;
+    });
+    this.canvas.addEventListener("touchmove", (e) => {
+      if(e.changedTouches[0].pageX - this.touchStartX >this.swipeDistance){
+        this.player.startCharge()
+
+      }
+
     });
   }
-  resize(width, height) {   
+  resize(width, height) {
     this.canvas.width = width;
     this.canvas.height = height;
     // states get reset when the canvas is resized so its better to change state only when resized for performance reasons
-    this.ctx.fillStyle = "red";
+    this.ctx.fillStyle = "blue";
     this.ctx.font = "15px Bungee";
-    this.ctx.textAlign='right'
-
+    this.ctx.textAlign = "right";
+    this.ctx.strokeStyle = "white";
+    this.ctx.lineWidth = 3;
 
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.ratio = this.height / this.baseHeight;
     this.speed = 2 * this.ratio;
+    this.minSpeed=this.speed
+    this.maxSpeed=this.speed*5
     this.gravity = 0.15 * this.ratio;
 
     this.background.resize();
@@ -63,15 +95,20 @@ class Game {
 
     this.score = 0;
     this.gameOver = false;
-    this.timer=0
+    this.timer = 0;
+    this.barSize=10*this.ratio
   }
   render(deltaTime) {
-    if(deltaTime && !this.gameOver){
-        this.timer += deltaTime
+    if (deltaTime){
+
+      this.handlePeriodicEvents(deltaTime)
+      if (!this.gameOver) {
+        this.timer += deltaTime;
+      }
     }
     this.background.update();
     this.background.draw();
-    this.drawStatusText()
+    this.drawStatusText();
     this.player.update();
     this.player.draw();
     this.obstacles.forEach((obstacle) => {
@@ -82,25 +119,76 @@ class Game {
   createObstacles() {
     this.obstacles = [];
     //deletes all the previous obstacles
-    const firstX = 100;
+    const firstX = 600;
     const obstacleSpacing = 600 * this.ratio;
     for (let i = 0; i < this.numberOfObstacles; i++) {
       this.obstacles.push(new Obstacle(this, firstX + i * obstacleSpacing));
     }
   }
-  formatTimer(){
-    return (this.timer * 0.001).toFixed(2) }
-  drawStatusText(){
-    this.ctx.save()
-    this.ctx.fillText('Score: '+ this.score,this.width-10,30)
-    this.ctx.textAlign='left'
-    this.ctx.fillText('Time: '+ this.formatTimer(),10,30)
-    if(this.gameOver){
-        this.ctx.textAlign='center'
-        this.ctx.font='30px Bungee'
-        this.ctx.fillText('GAME OVER',this.width*0.5,this.height*0.5)
+  checkCollision(a, b) {
+    const dx = a.collisionX - b.collisionX;
+    const dy = a.collisionY - b.collisionY;
+    const distance = Math.hypot(dx, dy);
+    const sumOfRadii = a.collisionRadius + b.collisionRadius;
+    return distance < sumOfRadii;
+  }
+  formatTimer() {
+    return (this.timer * 0.001).toFixed(2);
+  }
+  handlePeriodicEvents(deltaTime){
+    if(this.eventTimer<this.eventInterval){
+      this.eventTimer+=deltaTime
+      this.eventUpdate=false
     }
-    this.ctx.restore()
+    else{
+      this.eventTimer %= this.eventInterval;
+      this.eventUpdate=true
+    }
+  }
+
+  drawStatusText() {
+    this.ctx.save();
+    this.ctx.fillText("Score: " + this.score, this.width - 10, 30);
+    this.ctx.textAlign = "left";
+    this.ctx.fillText("Time: " + this.formatTimer(), 10, 30);
+    if (this.gameOver) {
+      if (this.player.collided) {
+        this.message1 = "Getting Rusty Are We";
+        this.message2 = "Collision Timer: " + this.formatTimer() + "seconds!";
+      } else if (this.obstacles.length <= 0) {
+        this.message1 = "A Job Well Done";
+        this.message2 =
+          "Can you do it faster than " + this.formatTimer() + "seconds?";
+      }
+      this.ctx.textAlign = "center";
+      this.ctx.font = "30px Bungee";
+      this.ctx.fillText(
+        this.message1,
+        this.width * 0.5,
+        this.height * 0.5 - 20
+      );
+      this.ctx.font = "15px Bungee";
+
+      this.ctx.fillText(this.message2, this.width * 0.5, this.height * 0.5);
+      this.ctx.fillText(
+        'Press "R" to try agian',
+        this.width * 0.5,
+        this.height * 0.5 + 20
+      );
+    }
+    
+   if(this.player.energy<=25){
+      this.ctx.fillStyle='red'
+    }
+    else{
+      this.ctx.fillStyle='green'
+
+    }
+    for (let i = 0; i < this.player.energy; i++) {
+        this.ctx.fillRect(10+ this.barSize*i,40,this.barSize+0.5,15)
+        
+    }
+    this.ctx.restore();
   }
 }
 
@@ -111,10 +199,10 @@ window.addEventListener("load", function () {
   canvas.width = 720;
   const game = new Game(canvas, ctx);
   game.render();
-  let lastTime=0
+  let lastTime = 0;
   function animate(timeStamp) {
-   const deltaTime= timeStamp-lastTime
-    lastTime=timeStamp
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     game.render(deltaTime);
     requestAnimationFrame(animate);
--- assets/scripts/player.js
+++ assets/scripts/player.js
@@ -9,17 +9,46 @@ class Player {
     this.width;
     this.speedY;
     this.flapSpeed;
+    this.collisionX = this.x;
+    this.collisionY;
+    this.collisionRadius;
+    this.collided;
+    this.energy = 30;
+    this.maxEnergy = this.energy * 2;
+    this.minEnergy = 15;
+    this.charging;
+    this.image=document.getElementById("fish")
+    this.imgHeight=this.image.height
+    this.imgWidth=this.image.width
+    this.frameY
   }
   draw() {
-    this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
-  }
+    // this.game.ctx.strokeRect(this.x, this.y, this.width, this.height);
+    // this.game.ctx.beginPath();
+    // this.game.ctx.arc(
+    //   this.collisionX + this.width * 0.5,
+    //   this.collisionY + this.height * 0.5,
+    //   this.collisionRadius,
+    //   0,
+    //   2 * Math.PI
+    //   );
+    //   this.game.ctx.stroke();
+      this.game.ctx.drawImage(this.image,0,0,this.spriteWidth,this.spriteHeight,this.x, this.y,this.width, this.height)
+    }
   update() {
+    this.handleEnergy();
+
     this.y += this.speedY;
+
+    this.collisionY = this.y;
     if (!this.isTouchingBottom()) {
       this.speedY += this.game.gravity;
     }
     if (this.isTouchingBottom()) {
       this.y = this.game.height - this.height;
+    }
+    if (this.isTouchingTop()){
+      this.y=0
     }
   }
   resize() {
@@ -28,13 +57,39 @@ class Player {
     this.height = this.spriteHeight * this.game.ratio;
     this.y = this.game.height * 0.5 - this.height * 0.5;
     this.speedY = -5 * this.game.ratio;
-    this.flapSpeed=5*this.game.ratio
+    this.flapSpeed = 5 * this.game.ratio;
+    this.collisionRadius = this.width * 0.5;
+    this.collided = false;
+    this.charging=false
+    this.frameY=0
+  }
+  startCharge() {
+    this.charging = true;
+    this.game.speed = this.game.maxSpeed;
+  }
+  stopCharge() {
+    this.charging = false;
+    this.game.speed = this.game.minSpeed;
   }
   isTouchingTop() {
     return this.y < 0;
   }
   isTouchingBottom() {
     return this.y > this.game.height - this.height;
+  }
+  handleEnergy() {
+    if (this.game.eventUpdate) {
+      if (this.energy < this.maxEnergy) {
+        this.energy += 1;
+      }
+      if (this.charging) {
+        this.energy -= 12;
+        if (this.energy <= 0) {
+          this.energy = 0;
+          this.stopCharge();
+        }
+      }
+    }
   }
   flap() {
     if (!this.isTouchingTop()) {
